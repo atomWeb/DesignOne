4cb516c47f20485307bbaacc87c515d03892ec3f
nabusim <cristianjose@gmail.com>
2024-11-01 08:20:46 +0100

    Initial Commit

---
+++ DesignOne/DesignOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignOneApp.swift
+//  DesignOne
+//
+//  Created by Cristian Jose Perez Guerra on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
